555e8055d58465e41357444bf9a0cac7936f4e04
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-18 18:10:20 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+from docs import getVersion
+
+
+changelog = open('CHANGES.rst').read()
+long_description = "\n\n".join([
+    open('README.rst').read(),
+    open('CONTRIBUTORS.rst').read(),
+    changelog
+])
+
+
+setup(
+    name='marcxml_parser',
+    version=getVersion(changelog),
+    description="MARC XML / OAI parser.",
+    long_description=long_description,
+    url='https://github.com/edeposit/marcxml_parser',
+
+    author='Edeposit team',
+    author_email='edeposit@email.cz',
+
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+    license='MIT',
+
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+
+    # scripts=[''],
+
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        "pyDHTMLParser>=2.0.7",
+    ],
+    extras_require={
+        "test": [
+            "pytest"
+        ],
+        "docs": [
+            "sphinx",
+            "sphinxcontrib-napoleon",
+        ]
+    },
+)
