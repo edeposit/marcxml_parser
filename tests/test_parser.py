d137acf611708a3e2b199c7776cc71c8af27231b
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-20 16:39:44 +0100

    #5: Added few tests of the code, preparation for refactoring.

---
+++ tests/test_parser.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import os
+import os.path
+import glob
+
+import pytest
+
+from marcxml_parser import marcxml
+
+
+# Variables ===================================================================
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+
+
+# Functions & classes =========================================================
+@pytest.fixture
+def aleph_files():
+    files = glob.glob(DATA_DIR + "/aleph_*.xml")
+
+    return map(lambda x: os.path.abspath(x), files)
+
+
+# Tests =======================================================================
+def test_order():
+    xml = """<record xmlns="http://www.loc.gov/MARC21/slim/"
+xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+xsi:schemaLocation="http://www.loc.gov/MARC21/slim
+http://www.loc.gov/standards/marcxml/schema/MARC21slim.xsd">
+
+
+<datafield tag="650" ind1="0" ind2="7">
+<subfield code="a">programování</subfield>
+<subfield code="7">ph115891</subfield>
+<subfield code="2">czenas</subfield>
+</datafield>
+</record>
+"""
+    parsed = marcxml.MARCXMLRecord(xml)
+
+    assert xml == parsed.__str__()
+
+
+def _test_input_output(aleph_files):
+    for fn in aleph_files:
+        with open(fn) as f:
+            data = f.read()
+
+        parsed = marcxml.MARCXMLRecord(data)
+
+        with open("orig.xml", "w") as f:
+            f.write(data)
+        with open("parsed.xml", "w") as f:
+            f.write(parsed.__str__())
+
+        assert parsed.__str__().strip() == data.strip()
