3ee57e2359e4e25860b84fbd1f2f744d0d070351
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-26 14:38:14 +0100

    #5: Added import tests for package.

---
+++ tests/test_module.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Tests =======================================================================
+def test_module_imports():
+    from marcxml_parser import Person
+    from marcxml_parser import Corporation
+    from marcxml_parser import MARCSubrecord
+
+    from marcxml_parser import MARCXMLParser
+    from marcxml_parser import MARCXMLRecord
+    from marcxml_parser import MARCXMLQuery
