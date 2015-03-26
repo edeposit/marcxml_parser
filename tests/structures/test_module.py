59ecf8be6941d9b6b4b404452024ad59301f1653
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-26 14:28:12 +0100

    #5: Added tests of the structures/ subpackage __init__.py.

---
+++ tests/structures/test_module.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Tests =======================================================================
+def test_imports():
+    from marcxml_parser.structures import Person
+    from marcxml_parser.structures import Corporation
+    from marcxml_parser.structures import MARCSubrecord
