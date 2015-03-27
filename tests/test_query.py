a62b55cbade54d6c055fd3a1d942b29e6dd3a224
Bystroushaak <bystrousak@kitakitsune.org>
2015-03-27 11:40:32 +0100

    #5: Added tests of .get_urls() and .get_internal_urls(). Fixed #11.

--- tests/test_query.py
+++ tests/test_query.py
@@ -10,12 +10,28 @@ from marcxml_parser import Person
 from marcxml_parser.query import MARCXMLQuery
 
 from test_parser import unix_file
+from test_serializer import aleph_files
 
 
 # Functions & classes =========================================================
 @pytest.fixture
+def epub_file():
+    files = aleph_files()
+
+    filename = next((x for x in files if "epub" in x))
+
+    with open(filename) as f:
+        return f.read()
+
+
+@pytest.fixture
 def parsed():
     return MARCXMLQuery(unix_file())
+
+
+@pytest.fixture
+def epub():
+    return MARCXMLQuery(epub_file())
 
 
 # Tests =======================================================================
@@ -88,3 +104,28 @@ def test_get_binding(parsed):
 
 def test_get_originals(parsed):
     assert parsed.get_originals() == ["Art of UNIX programming"]
+
+
+def test_get_urls(parsed):
+    assert parsed.get_urls() == []
+
+
+# Tests of epub file ==========================================================
+def test_epub_get_urls(epub):
+    assert epub.get_urls() == []
+
+
+def test_epub_get_internal_urls(epub):
+    base_url = "http://edeposit-test.nkp.cz"
+    path = "/producents/nakladatelstvi-delta/epublications/echa-201"
+
+    url = base_url + path
+
+    assert epub.get_internal_urls() == [
+        url + "0-2011/echa-2010-2011-eva-jelinkova-michael-spirit-eds.pdf",
+        url + "0-2011-1/echa-2010-2011-eva-jelinkova-michael-spirit-eds.epub",
+        url + "0-2011-1/echa-2010-2011-eva-jelinkova-michael-spirit-eds.mobi",
+        url + "2-1/echa-2012-eva-jelinkova-michael-spirit-eds.mobi",
+        url + "0-2011/echa-2013-eva-jelinkova-michael-spirit-eds.epub",
+        "http://aleph.nkp.cz/F/?func=direct&doc_number=000003059&local_base=CZE-DEP"
+    ]
