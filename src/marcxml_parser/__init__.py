3495a749e35b5b8250a96c5092f707205ddaae01
Bystroushaak <bystrousak@kitakitsune.org>
2015-04-24 10:42:20 +0200

    #20: Added record.record_iterator().

--- src/marcxml_parser/__init__.py
+++ src/marcxml_parser/__init__.py
@@ -5,9 +5,10 @@
 #
 # Imports =====================================================================
 from .record import MARCXMLRecord
+from .record import record_iterator
 
-from structures import Person
-from structures import Corporation
-from structures import PublicationType
+from .structures import Person
+from .structures import Corporation
+from .structures import PublicationType
 
-from structures import MARCSubrecord
+from .structures import MARCSubrecord
--- src/marcxml_parser/record.py
+++ src/marcxml_parser/record.py
@@ -4,6 +4,8 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+import dhtmlparser
+
 from .query import MARCXMLQuery
 
 
@@ -15,3 +17,24 @@ class MARCXMLRecord(MARCXMLQuery):
     """
     def __init__(self, xml=None, resort=True):
         super(MARCXMLRecord, self).__init__(xml, resort)
+
+
+def record_iterator(xml):
+    """
+    Iterate over all ``<record>`` tags in `xml`.
+
+    Args:
+        xml (str): Input string with XML. UTF-8 is prefered encoding, unicode
+                   should be ok.
+
+    Yields:
+        MARCXMLRecord: For each corresponding ``<record>``.
+    """
+    dom = None
+    try:
+        dom = dhtmlparser.parseString(xml)
+    except UnicodeError:
+        dom = dhtmlparser.parseString(xml.encode("utf-8"))
+
+    for record_xml in dom.findB("record"):
+        yield MARCXMLRecord(record_xml)
